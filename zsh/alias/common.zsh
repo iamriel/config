666a93a7bd452ccc00d8968207cf105416bd8d75
Rieljun Liguid <me@iamriel.com>
2017-11-10 20:19:20 +0800

    Configure org agenda and add aliases

--- zsh/alias/common.zsh
+++ zsh/alias/common.zsh
@@ -1,1 +1,3 @@
 alias runserver="./manage.py runserver;"
+alias showFiles="defaults write com.apple.finder AppleShowAllFiles -boolean true ; killall Finder;"
+alias hideFiles="defaults write com.apple.finder AppleShowAllFiles -boolean false ; killall Finder"
